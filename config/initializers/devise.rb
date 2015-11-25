e8bc95fcd751460d8dd60f46a0e22d06d94a3de5
BS iMac <benni.singer@gmail.com>
2015-11-25 15:36:18 +0100

    Little manual edits for Devise to run properly (according to Terminal output after Devise install)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -109,6 +109,8 @@ Devise.setup do |config|
   # the user cannot access the website without confirming their account.
   # config.allow_unconfirmed_access_for = 2.days
 
+  # ----------------- CHANGE (top) ? -------------------
+
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
   # their account within 3 days after the mail was sent, but on the fourth day
@@ -116,6 +118,8 @@ Devise.setup do |config|
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
   # config.confirm_within = 3.days
+
+  # ----------------- CHANGE (top) ? -------------------
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
