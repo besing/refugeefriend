223913b9670aa7411b2dae05a621a1110437d040
BS iMac <benni.singer@gmail.com>
2015-11-25 17:02:01 +0100

    - Modify User Migration (after db:migrate change-files needed, original Migration file can only be run once) - Detail Profile Info (Language, Interests..) still unimplemented --> separate Model with Associations - Model: added "confirmable" according to SN-Tutorial

--- db/migrate/20151125143623_devise_create_users.rb
+++ db/migrate/20151125143623_devise_create_users.rb
@@ -2,8 +2,14 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :refugee_status
+      t.boolean :sex
+      t.integer :age
+      t.string :avatar
+      ## Basic Infos (detailed infos in separate Models)
 
       ## Recoverable
       t.string   :reset_password_token
@@ -16,14 +22,14 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      t.inet     :current_sign_in_ip # Devise default; set as string in SN-Tutorial (?)
+      t.inet     :last_sign_in_ip # Devise default; set as string in SN-Tutorial (?)
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable (requires any email changes to be confirmed)
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -36,7 +42,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
